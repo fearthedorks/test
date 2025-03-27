f7810f149f121c1ac0e24f8d9593d5350fa4e049
fearthedorks <nykolopez250@gmail.com>
2025-03-27 17:07:10 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -39,7 +39,7 @@ ScreenGui.ResetOnSpawn = false
 ScreenGui.Enabled = false
  
 local BackFrame = Instance.new("Frame")
-BackFrame.Size = UDim2.new(1, 0, 1, 0)
+BackFrame.Size = UDim2.new(1, 0, 0.5, 0)
 
 BackFrame.AnchorPoint = Vector2.new(0.5, 0.5)
 BackFrame.Position = UDim2.new(0.5, 0, 0.5, 0)
@@ -75,7 +75,8 @@ Loading.Position = UDim2.fromScale(0.5, 0.2)
 Corner:Clone().Parent = Loading
  
 local Frame = Instance.new("ScrollingFrame")
-Frame.Size = UDim2.new(0.8, 3, 1.2, 1)
+Frame.Size = UDim2.new(0.8, 0, 0.5, 0)
+
 Frame.CanvasSize = UDim2.new(0, 0, 0, 0)
 Frame.AutomaticCanvasSize = Enum.AutomaticSize.Y
 Frame.ScrollingDirection = Enum.ScrollingDirection.Y
